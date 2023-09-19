e9c4ea92867e73d5f1a3149087acfc06e06458ac
awan-ama <115125091+awan-ama@users.noreply.github.com>
2023-09-19 22:18:13 +0800

    Add files via upload

---
+++ Modul 1/src/PRAK102_2210817210012_MuhammadRakaAzwar.java
@@ -0,0 +1,11 @@
+public class PRAK102_2210817210012_MuhammadRakaAzwar {
+    public static void main(String[] args) {
+    System.out.println("##############################################");
+    System.out.println("#                                            #");
+    System.out.println("#            Muhammad Raka Azwar             #");
+    System.out.println("#               2210817210012                #");
+    System.out.println("#                                            #");
+    System.out.println("##############################################");
+    }
+}
+
